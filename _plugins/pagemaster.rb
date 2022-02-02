e0c0e5047cc501f8465c496f586ba4f7e3473415
Slava Romanov <davinel000@gmail.com>
2022-02-02 19:22:15 +0100

    removed pagemaster

---
+++ _plugins/pagemaster.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# stdlib
+require 'csv'
+require 'fileutils'
+require 'json'
+require 'jekyll'
+
+# 3rd party
+require 'rainbow'
+require 'safe_yaml'
+
+# relative
+require 'pagemaster/collection'
+require 'pagemaster/command'
+require 'pagemaster/error'
+require 'pagemaster/site'
+require 'pagemaster/version'
+
+#
+#
+module Pagemaster; end
